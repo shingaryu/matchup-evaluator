d4798ff0c9399fbb3d99af6805bc566ecd7c6de2
shingaryu <shingaryu@gmail.com>
2021-03-08 00:34:29 +0100

    Fix postgres connection error was not handled

--- repositories/postgresRepositoryBase.js
+++ repositories/postgresRepositoryBase.js
@@ -2,6 +2,8 @@ const { Client } = require('pg')
 
 class PostgresRepositoryBase {
   constructor() {
+    this.connected = false;
+
     if (process.env.DATABASE_URL.indexOf('localhost') >= 0) {
       this.client = new Client({
         connectionString: process.env.DATABASE_URL,
@@ -14,28 +16,42 @@ class PostgresRepositoryBase {
         }
       });
     }
-
-    this.client.connect();
   }
 
-  endConnection() {
+  async initConnection() {
+    console.log("establish postgres connection...")
+    try {
+      await this.client.connect();
+      console.log("postgres connection established!")
+      this.connected = true;
+    } catch (e) {
+      console.log("failed to create postgres connection")
+      this.connected = false;     
+      throw (e);
+    }
+  }
+
+  async endConnection() {
     console.log("postgres end connection...")
-    this.client.end();
+    return this.client.end();
   }
 
   sqlQueryPromise(statement) {
-    // this.client.connect();
     return new Promise((resolve, reject) => {
-      this.client.query(statement, (err, res) => {
-        if (err) {
-          // this.client.end();
-          console.log(err)
-          reject(err);
-        } else {
-          // this.client.end();
-          resolve(res.rows);
-        }
-      })
+      // not concurrency safe?
+      const initPromise = this.connected ? Promise.resolve(): this.initConnection();
+      initPromise.then(() => {
+        this.client.query(statement, (err, res) => {
+          if (err) {
+            console.log(err)
+            reject(err);
+          } else {
+            resolve(res.rows);
+          }
+        })
+      }, initError => {
+        reject(initError)
+      });
     });
   }
 }
--- scripts/start-cluster-evaluation.ts
+++ scripts/start-cluster-evaluation.ts
@@ -17,64 +17,73 @@ const retryingRate = 5000; //ms
 
 throng({
   master: async () => {
-    if (!commanderProgram.asWorker) {
-      process.once('SIGINT', async () => {
-        console.log(`master disconnect`)
-        await sqlService.endConnection();
-        process.exit()
-      })
-
-      process.once('SIGTERM', async () => {
-        console.log(`master disconnect`)
-        await sqlService.endConnection();
-        process.exit()
-      })
-
-      console.log(`Start master`);
-      const start = new Date();
-      await evaluationQueueApi.postReset();
-
-      // need to run this on event loop, in order to exit master function and start forking workers 
-      setTimeout(async () => {      
-        while(true) {
-          try {
-            const targets = await (await evaluationQueueApi.getTargetsList()).data;
-            const processes = await (await evaluationQueueApi.getProcessingList()).data;
-            console.log(`${targets.length} evaluations in todo, ${processes.length} evaluations in progress`)
-            if (targets.length === 0 && processes.length === 0) {
-              console.log('all evaluation completed!');
-              console.log(`elapsed time: ${(new Date().getTime() - start.getTime()) / 1000} sec`);
-              // clearInterval(interval);
-              await sqlService.endConnection();
-              console.log(`process exits...`);
-              process.exit();
-            } else {
-              await new Promise(r => setTimeout(r, fetchingRate));
+    try {
+      if (!commanderProgram.asWorker) {
+        process.once('SIGINT', async () => {
+          console.log(`master disconnect`)
+          process.exit()
+        })
+  
+        process.once('SIGTERM', async () => {
+          console.log(`master disconnect`)
+          process.exit()
+        })
+  
+        console.log(`Start master`);
+        const start = new Date();
+        await evaluationQueueApi.postReset();
+  
+        // need to run this on event loop, in order to exit master function and start forking workers 
+        setTimeout(async () => {      
+          while(true) {
+            try {
+              const targets = await (await evaluationQueueApi.getTargetsList()).data;
+              const processes = await (await evaluationQueueApi.getProcessingList()).data;
+              console.log(`${targets.length} evaluations in todo, ${processes.length} evaluations in progress`)
+              if (targets.length === 0 && processes.length === 0) {
+                console.log('all evaluation completed!');
+                console.log(`elapsed time: ${(new Date().getTime() - start.getTime()) / 1000} sec`);
+                // clearInterval(interval);
+                console.log(`process exits...`);
+                process.exit();
+              } else {
+                await new Promise(r => setTimeout(r, fetchingRate));
+              }
+            } catch (e) {
+              console.log(`Error while fetching evaluation status on master. Retrying in ${retryingRate / 1000} sec...`);
+              await new Promise(r => setTimeout(r, retryingRate));
             }
-          } catch (e) {
-            console.log(`Error while fetching evaluation status on master. Retrying in ${retryingRate / 1000} sec...`);
-            await new Promise(r => setTimeout(r, retryingRate));
           }
-        }
-      }, 1000);
+        }, 1000);
+      }
+    } catch (e) {
+      console.log(e.stack);
+      console.log(`error with master`);
+      console.log(`process exits...`);
+      process.exit();
     }
   },
-  worker: async(id: number, disconnect: any) => {
-    console.log(`Start worker ${id}`);
-    process.once('SIGINT', async () => {
-      console.log(`worker ${id} disconnect`)
+  worker: async(id: number, disconnect: any) => {    
+    try {
+      console.log(`Start worker ${id}`);
+      process.once('SIGINT', async () => {
+        console.log(`worker ${id} disconnect`)
+        await sqlService.endConnection();
+        disconnect();
+      })
+  
+      process.once('SIGTERM', async () => {
+        console.log(`worker ${id} disconnect`)
+        await sqlService.endConnection();
+        disconnect();
+      })
+      await sqlService.initConnection();
+      await calcAsWorker(weights, commanderProgram.numoftrials, commanderProgram.depth, 1);
       await sqlService.endConnection();
-      disconnect();
-    })
-
-    process.once('SIGTERM', async () => {
-      console.log(`worker ${id} disconnect`)
-      await sqlService.endConnection();
-      disconnect();
-    })
-
-    await calcAsWorker(weights, commanderProgram.numoftrials, commanderProgram.depth, 1);
-    await sqlService.endConnection();
+    } catch (e) {
+      console.log(e.stack);
+      console.log(`error with worker ${id}`);
+    }
     console.log(`worker ${id} exits...`)
     // disconnect();
   },
